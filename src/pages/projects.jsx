2b650726a60da7b7339c286d4efce45ece57e727
James Daggett <james.v.daggett@gmail.com>
2023-04-27 17:45:25 -0400

    base modification of site

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -62,14 +62,14 @@ export default function Projects() {
   return (
     <>
       <Head>
-        <title>Projects - Spencer Sharp</title>
+        <title>Projects - Uley</title>
         <meta
           name="description"
-          content="Things I’ve made trying to put my dent in the universe."
+          content="Things I’ve made trying to learn to create my own game."
         />
       </Head>
       <SimpleLayout
-        title="Things I’ve made trying to put my dent in the universe."
+        title="Things I’ve made trying to learn to create my own game."
         intro="I’ve worked on tons of little projects over the years but these are the ones that I’m most proud of. Many of them are open-source, so if you see something that piques your interest, check out the code and contribute if you have ideas for how it can be improved."
       >
         <ul
